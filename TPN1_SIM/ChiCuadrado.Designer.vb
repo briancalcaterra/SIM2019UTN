66ed3f90153ed93da61ffef4c1c77a90cc8d98b0
GeorgiiGonzalez <gg.georginagonzalez@gmail.com>
2019-04-02 15:00:56 -0300

    subo punto b

--- TPN1_SIM/ChiCuadrado.Designer.vb
+++ TPN1_SIM/ChiCuadrado.Designer.vb
@@ -23,25 +23,19 @@ Partial Class ChiCuadrado
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ChiCuadrado))
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
-        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series3 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim Series4 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txt_intervalos = New System.Windows.Forms.MaskedTextBox()
         Me.txt_cantidad_muestras = New System.Windows.Forms.MaskedTextBox()
-        Me.rbtn_veinte = New System.Windows.Forms.RadioButton()
-        Me.rbtn_diez = New System.Windows.Forms.RadioButton()
-        Me.rbtn_cinco = New System.Windows.Forms.RadioButton()
         Me.btn_graficar = New System.Windows.Forms.Button()
         Me.btn_limpiar = New System.Windows.Forms.Button()
         Me.grilla_chi_cuadrado = New System.Windows.Forms.DataGridView()
-        Me.grafico_chi_cuadrado = New System.Windows.Forms.DataVisualization.Charting.Chart()
-        Me.grilla_serie = New System.Windows.Forms.DataGridView()
-        Me.col_numero = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.col_num = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Intervalos = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Col_Fo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Col_fe = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -49,9 +43,14 @@ Partial Class ChiCuadrado
         Me.Col_Cuadrado = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Col_EstadisticoCalculado = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Col_EstadisticoAcum = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.grafico_chi_cuadrado = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.grilla_serie = New System.Windows.Forms.DataGridView()
+        Me.col_numero = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.col_num = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
+        Me.lbl_GradosDeLibrtad = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         CType(Me.grilla_chi_cuadrado, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.grafico_chi_cuadrado, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -96,10 +95,8 @@ Partial Class ChiCuadrado
         'GroupBox1
         '
         Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GroupBox1.Controls.Add(Me.txt_intervalos)
         Me.GroupBox1.Controls.Add(Me.txt_cantidad_muestras)
-        Me.GroupBox1.Controls.Add(Me.rbtn_veinte)
-        Me.GroupBox1.Controls.Add(Me.rbtn_diez)
-        Me.GroupBox1.Controls.Add(Me.rbtn_cinco)
         Me.GroupBox1.Controls.Add(Me.Label7)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
@@ -114,50 +111,23 @@ Partial Class ChiCuadrado
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Datos: "
         '
+        'txt_intervalos
+        '
+        Me.txt_intervalos.Location = New System.Drawing.Point(423, 33)
+        Me.txt_intervalos.Mask = "999999"
+        Me.txt_intervalos.Name = "txt_intervalos"
+        Me.txt_intervalos.Size = New System.Drawing.Size(152, 27)
+        Me.txt_intervalos.TabIndex = 11
+        Me.txt_intervalos.ValidatingType = GetType(Integer)
+        '
         'txt_cantidad_muestras
         '
         Me.txt_cantidad_muestras.Location = New System.Drawing.Point(147, 33)
-        Me.txt_cantidad_muestras.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.txt_cantidad_muestras.Mask = "999999"
+        Me.txt_cantidad_muestras.Margin = New System.Windows.Forms.Padding(4)
+        Me.txt_cantidad_muestras.Mask = "9999999"
         Me.txt_cantidad_muestras.Name = "txt_cantidad_muestras"
         Me.txt_cantidad_muestras.Size = New System.Drawing.Size(144, 27)
         Me.txt_cantidad_muestras.TabIndex = 1
-        '
-        'rbtn_veinte
-        '
-        Me.rbtn_veinte.AutoSize = True
-        Me.rbtn_veinte.Location = New System.Drawing.Point(597, 36)
-        Me.rbtn_veinte.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.rbtn_veinte.Name = "rbtn_veinte"
-        Me.rbtn_veinte.Size = New System.Drawing.Size(48, 23)
-        Me.rbtn_veinte.TabIndex = 4
-        Me.rbtn_veinte.TabStop = True
-        Me.rbtn_veinte.Text = "20"
-        Me.rbtn_veinte.UseVisualStyleBackColor = True
-        '
-        'rbtn_diez
-        '
-        Me.rbtn_diez.AutoSize = True
-        Me.rbtn_diez.Location = New System.Drawing.Point(508, 36)
-        Me.rbtn_diez.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.rbtn_diez.Name = "rbtn_diez"
-        Me.rbtn_diez.Size = New System.Drawing.Size(48, 23)
-        Me.rbtn_diez.TabIndex = 3
-        Me.rbtn_diez.TabStop = True
-        Me.rbtn_diez.Text = "10"
-        Me.rbtn_diez.UseVisualStyleBackColor = True
-        '
-        'rbtn_cinco
-        '
-        Me.rbtn_cinco.AutoSize = True
-        Me.rbtn_cinco.Location = New System.Drawing.Point(427, 36)
-        Me.rbtn_cinco.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.rbtn_cinco.Name = "rbtn_cinco"
-        Me.rbtn_cinco.Size = New System.Drawing.Size(39, 23)
-        Me.rbtn_cinco.TabIndex = 2
-        Me.rbtn_cinco.TabStop = True
-        Me.rbtn_cinco.Text = "5"
-        Me.rbtn_cinco.UseVisualStyleBackColor = True
         '
         'btn_graficar
         '
@@ -168,7 +138,7 @@ Partial Class ChiCuadrado
         Me.btn_graficar.Image = CType(resources.GetObject("btn_graficar.Image"), System.Drawing.Image)
         Me.btn_graficar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.btn_graficar.Location = New System.Drawing.Point(739, 27)
-        Me.btn_graficar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_graficar.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_graficar.Name = "btn_graficar"
         Me.btn_graficar.Size = New System.Drawing.Size(163, 55)
         Me.btn_graficar.TabIndex = 0
@@ -185,7 +155,7 @@ Partial Class ChiCuadrado
         Me.btn_limpiar.Image = CType(resources.GetObject("btn_limpiar.Image"), System.Drawing.Image)
         Me.btn_limpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.btn_limpiar.Location = New System.Drawing.Point(934, 27)
-        Me.btn_limpiar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_limpiar.Margin = New System.Windows.Forms.Padding(4)
         Me.btn_limpiar.Name = "btn_limpiar"
         Me.btn_limpiar.Size = New System.Drawing.Size(159, 55)
         Me.btn_limpiar.TabIndex = 1
@@ -201,51 +171,8 @@ Partial Class ChiCuadrado
         Me.grilla_chi_cuadrado.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.grilla_chi_cuadrado.Name = "grilla_chi_cuadrado"
         Me.grilla_chi_cuadrado.RowTemplate.Height = 24
-        Me.grilla_chi_cuadrado.Size = New System.Drawing.Size(873, 303)
+        Me.grilla_chi_cuadrado.Size = New System.Drawing.Size(1009, 303)
         Me.grilla_chi_cuadrado.TabIndex = 20
-        '
-        'grafico_chi_cuadrado
-        '
-        ChartArea1.Name = "ChartArea1"
-        Me.grafico_chi_cuadrado.ChartAreas.Add(ChartArea1)
-        Legend1.Name = "Legend1"
-        Me.grafico_chi_cuadrado.Legends.Add(Legend1)
-        Me.grafico_chi_cuadrado.Location = New System.Drawing.Point(12, 512)
-        Me.grafico_chi_cuadrado.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.grafico_chi_cuadrado.Name = "grafico_chi_cuadrado"
-        Series1.ChartArea = "ChartArea1"
-        Series1.Legend = "Legend1"
-        Series1.Name = "Observado"
-        Series2.ChartArea = "ChartArea1"
-        Series2.Legend = "Legend1"
-        Series2.Name = "Esperado"
-        Me.grafico_chi_cuadrado.Series.Add(Series1)
-        Me.grafico_chi_cuadrado.Series.Add(Series2)
-        Me.grafico_chi_cuadrado.Size = New System.Drawing.Size(873, 309)
-        Me.grafico_chi_cuadrado.TabIndex = 21
-        Me.grafico_chi_cuadrado.Text = "Chart1"
-        '
-        'grilla_serie
-        '
-        Me.grilla_serie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.grilla_serie.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.col_numero, Me.col_num})
-        Me.grilla_serie.Location = New System.Drawing.Point(952, 133)
-        Me.grilla_serie.Name = "grilla_serie"
-        Me.grilla_serie.RowTemplate.Height = 24
-        Me.grilla_serie.Size = New System.Drawing.Size(240, 508)
-        Me.grilla_serie.TabIndex = 22
-        '
-        'col_numero
-        '
-        Me.col_numero.HeaderText = "Posicion"
-        Me.col_numero.Name = "col_numero"
-        Me.col_numero.Width = 60
-        '
-        'col_num
-        '
-        Me.col_num.HeaderText = "Numero"
-        Me.col_num.Name = "col_num"
-        Me.col_num.Width = 60
         '
         'Intervalos
         '
@@ -281,11 +208,56 @@ Partial Class ChiCuadrado
         '
         Me.Col_EstadisticoCalculado.HeaderText = "((fo- fe))^2/fe"
         Me.Col_EstadisticoCalculado.Name = "Col_EstadisticoCalculado"
+        Me.Col_EstadisticoCalculado.Width = 150
         '
         'Col_EstadisticoAcum
         '
         Me.Col_EstadisticoAcum.HeaderText = "Estadistico Acumulado"
         Me.Col_EstadisticoAcum.Name = "Col_EstadisticoAcum"
+        Me.Col_EstadisticoAcum.Width = 150
+        '
+        'grafico_chi_cuadrado
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.grafico_chi_cuadrado.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.grafico_chi_cuadrado.Legends.Add(Legend2)
+        Me.grafico_chi_cuadrado.Location = New System.Drawing.Point(29, 528)
+        Me.grafico_chi_cuadrado.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.grafico_chi_cuadrado.Name = "grafico_chi_cuadrado"
+        Series3.ChartArea = "ChartArea1"
+        Series3.Legend = "Legend1"
+        Series3.Name = "Observado"
+        Series4.ChartArea = "ChartArea1"
+        Series4.Legend = "Legend1"
+        Series4.Name = "Esperado"
+        Me.grafico_chi_cuadrado.Series.Add(Series3)
+        Me.grafico_chi_cuadrado.Series.Add(Series4)
+        Me.grafico_chi_cuadrado.Size = New System.Drawing.Size(958, 339)
+        Me.grafico_chi_cuadrado.TabIndex = 21
+        Me.grafico_chi_cuadrado.Text = "Chart1"
+        '
+        'grilla_serie
+        '
+        Me.grilla_serie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grilla_serie.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.col_numero, Me.col_num})
+        Me.grilla_serie.Location = New System.Drawing.Point(1077, 133)
+        Me.grilla_serie.Name = "grilla_serie"
+        Me.grilla_serie.RowTemplate.Height = 24
+        Me.grilla_serie.Size = New System.Drawing.Size(240, 698)
+        Me.grilla_serie.TabIndex = 22
+        '
+        'col_numero
+        '
+        Me.col_numero.HeaderText = "Posicion"
+        Me.col_numero.Name = "col_numero"
+        Me.col_numero.Width = 60
+        '
+        'col_num
+        '
+        Me.col_num.HeaderText = "Numero"
+        Me.col_num.Name = "col_num"
+        Me.col_num.Width = 60
         '
         'Label3
         '
@@ -303,7 +275,7 @@ Partial Class ChiCuadrado
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.Label4.Location = New System.Drawing.Point(28, 481)
+        Me.Label4.Location = New System.Drawing.Point(418, 507)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(72, 19)
         Me.Label4.TabIndex = 24
@@ -314,18 +286,30 @@ Partial Class ChiCuadrado
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.DeepSkyBlue
-        Me.Label5.Location = New System.Drawing.Point(948, 105)
+        Me.Label5.Location = New System.Drawing.Point(1073, 105)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(155, 19)
         Me.Label5.TabIndex = 25
         Me.Label5.Text = "Serie de Numeros:"
+        '
+        'lbl_GradosDeLibrtad
+        '
+        Me.lbl_GradosDeLibrtad.AutoSize = True
+        Me.lbl_GradosDeLibrtad.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_GradosDeLibrtad.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.lbl_GradosDeLibrtad.Location = New System.Drawing.Point(12, 462)
+        Me.lbl_GradosDeLibrtad.Name = "lbl_GradosDeLibrtad"
+        Me.lbl_GradosDeLibrtad.Size = New System.Drawing.Size(167, 19)
+        Me.lbl_GradosDeLibrtad.TabIndex = 26
+        Me.lbl_GradosDeLibrtad.Text = "Grados de Libertad:"
         '
         'ChiCuadrado
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.ClientSize = New System.Drawing.Size(1237, 878)
+        Me.ClientSize = New System.Drawing.Size(1408, 878)
+        Me.Controls.Add(Me.lbl_GradosDeLibrtad)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -356,13 +340,14 @@ Partial Class ChiCuadrado
     Friend WithEvents btn_limpiar As Button
     Friend WithEvents grilla_chi_cuadrado As DataGridView
     Friend WithEvents grafico_chi_cuadrado As DataVisualization.Charting.Chart
-    Friend WithEvents rbtn_veinte As System.Windows.Forms.RadioButton
-    Friend WithEvents rbtn_diez As System.Windows.Forms.RadioButton
-    Friend WithEvents rbtn_cinco As System.Windows.Forms.RadioButton
     Friend WithEvents txt_cantidad_muestras As System.Windows.Forms.MaskedTextBox
     Friend WithEvents grilla_serie As DataGridView
     Friend WithEvents col_numero As DataGridViewTextBoxColumn
     Friend WithEvents col_num As DataGridViewTextBoxColumn
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_intervalos As MaskedTextBox
     Friend WithEvents Intervalos As DataGridViewTextBoxColumn
     Friend WithEvents Col_Fo As DataGridViewTextBoxColumn
     Friend WithEvents Col_fe As DataGridViewTextBoxColumn
@@ -370,7 +355,5 @@ Partial Class ChiCuadrado
     Friend WithEvents Col_Cuadrado As DataGridViewTextBoxColumn
     Friend WithEvents Col_EstadisticoCalculado As DataGridViewTextBoxColumn
     Friend WithEvents Col_EstadisticoAcum As DataGridViewTextBoxColumn
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
+    Friend WithEvents lbl_GradosDeLibrtad As Label
 End Class
--- TPN1_SIM/ChiCuadrado.vb
+++ TPN1_SIM/ChiCuadrado.vb
@@ -1,7 +1,8 @@
-﻿Public Class ChiCuadrado
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+Public Class ChiCuadrado
 
     Dim bandera, var As Boolean
-    
+
 
     Private Sub ChiCuadrado_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -9,38 +10,82 @@
 
     Private Sub btn_graficar_Click(sender As Object, e As EventArgs) Handles btn_graficar.Click
         Validar_campos()
-        Dim cantidad As Double
+
+        Dim cant_muestras As Integer
         If var = False Then
             MsgBox("Ingrese todas las variables requeridas", MsgBoxStyle.OkOnly, "Error")
             Me.txt_cantidad_muestras.Focus()
         Else
-            Validar_enteros(cantidad)
-            cantidad = Convert.ToDouble(Me.txt_cantidad_muestras.Text)
-            If bandera = True Then
-                Generar_aleatorios(cantidad)
-            End If
+
+
+            Dim aleatorio, intervalos, fe, fila, j, est_acumulado As Double
+            Dim k, cont_frecuencia, fo As Integer
+            cant_muestras = Me.txt_cantidad_muestras.Text
+            Dim lista(0 To cant_muestras - 1) As Double
+            k = Me.txt_intervalos.Text
+            intervalos = Math.Round((1 / k), 4)
+            fe = cant_muestras / k
+
+            'aca comienza el generador de numeros aleatorios
+            Me.grilla_serie.Rows.Clear()
+            Me.grilla_chi_cuadrado.Rows.Clear()
+            Me.grafico_chi_cuadrado.Series(0).Points.Clear()
+            Me.grafico_chi_cuadrado.Series(1).Points.Clear()
+
+            For i As Decimal = 0 To (cant_muestras - 1)
+                aleatorio = Rnd()
+                grilla_serie.Rows.Add(i, FormatNumber(aleatorio, 4))
+                lista(i) = aleatorio
+            Next
+
+            fila = 0
+            'frecuencia observada
+            For j = 0 To (0.99) Step intervalos
+                fo = 0
+                For cont_frecuencia = 0 To (cant_muestras - 1) 'recorre la cantidad de muestras ingresadas por el usuario
+                    If j <= lista(cont_frecuencia) Then
+                        If lista(cont_frecuencia) < (j + intervalos) Then 'controla que el numero cargado en el vector este dentro de los limites de un intervalo
+                            fo += 1 'cuenta la cantidad de valores que aparecen dentro de un intervalo
+                        End If
+                    End If
+                Next cont_frecuencia
+                grafico_chi_cuadrado.Series("Observado").Points.AddXY((j + intervalos), fo) ''graficamos el valor observado en el intervalo j
+                grafico_chi_cuadrado.Series("Esperado").Points.AddXY((j + intervalos), fe) 'graficamos el valor del esperado en el intervalo j
+                'ahora agregamos una fila en la grilla por cada uno de los intervalos generados
+                grilla_chi_cuadrado.Rows.Add()
+                grilla_chi_cuadrado.Rows((fila)).Cells(0).Value = j.ToString + "-" + (j + intervalos).ToString
+                grilla_chi_cuadrado.Rows((fila)).Cells(1).Value = fo
+                grilla_chi_cuadrado.Rows((fila)).Cells(2).Value = fe
+                Dim resta As Double
+                resta = fo - fe
+                grilla_chi_cuadrado.Rows((fila)).Cells(3).Value = resta
+                grilla_chi_cuadrado.Rows((fila)).Cells(4).Value = resta * resta
+                Dim est_calculado As Double
+                est_calculado = (resta * resta) / fe
+                grilla_chi_cuadrado.Rows((fila)).Cells(5).Value = est_calculado
+                est_acumulado += est_calculado
+                grilla_chi_cuadrado.Rows((fila)).Cells(6).Value = est_acumulado
+                fila += 1
+            Next
+            grilla_chi_cuadrado.Rows.Add()
+            grilla_chi_cuadrado.Rows((fila)).Cells(5).Value = "Xi Cuadrado calculado"
+            grilla_chi_cuadrado.Rows((fila)).Cells(6).Value = est_acumulado.ToString
+
+
+            grafico_chi_cuadrado.Series("Observado").ChartType = SeriesChartType.Column
+            grafico_chi_cuadrado.Series("Observado").XValueType = ChartValueType.Double
+            grafico_chi_cuadrado.Series("Observado").YValueType = ChartValueType.Int32
+            grafico_chi_cuadrado.ChartAreas(0).AxisX.Interval = intervalos
+            grafico_chi_cuadrado.Series("Observado").IsVisibleInLegend = True
+
+            lbl_GradosDeLibrtad.Text = "Los grados de libertad son: " + (k - 1).ToString
+
+
+
         End If
     End Sub
 
-    Private Sub Generar_aleatorios(cant As Double)
-        Dim lista(0 To cant - 1) As Double
-        Dim aleatorio As Double
-        Dim i As Integer
-Randomize: Do While i <= (cant - 1)
-            aleatorio = Rnd()
-            lista(i) = FormatNumber(aleatorio, 4)
-            i = i + 1
-        Loop
-        Dim j As Integer
-        j = 0
-        Me.grilla_serie.Rows.Clear()
-        Do While (j <= cant - 1)
-            grilla_serie.Rows.Add(j, lista(j))
-            j = j + 1
-        Loop
 
-
-    End Sub
 
     Private Function Validar_enteros(x1) As Boolean
         If x1 >= 0 Then
@@ -54,9 +99,12 @@ Randomize: Do While i <= (cant - 1)
 
     Private Sub Limpiar()
         Me.txt_cantidad_muestras.Text = ""
+        Me.txt_intervalos.Text = ""
         Me.grilla_chi_cuadrado.Rows.Clear()
         Me.txt_cantidad_muestras.Focus()
         Me.grilla_serie.Rows.Clear()
+        Me.grafico_chi_cuadrado.Series(0).Points.Clear()
+        Me.grafico_chi_cuadrado.Series(1).Points.Clear()
 
     End Sub
 
@@ -64,8 +112,10 @@ Randomize: Do While i <= (cant - 1)
         Limpiar()
     End Sub
 
+
+
     Private Function Validar_campos() As Boolean
-        If (Me.txt_cantidad_muestras.Text = "" Or (rbtn_cinco.Checked = False And rbtn_diez.Checked = False And rbtn_veinte.Checked = False)) Then
+        If (Me.txt_cantidad_muestras.Text = "" Or Me.txt_intervalos.Text = "") Then
             var = False
         Else
             var = True
@@ -73,6 +123,6 @@ Randomize: Do While i <= (cant - 1)
         Return var
     End Function
 
-    
-   
+
+
 End Class
